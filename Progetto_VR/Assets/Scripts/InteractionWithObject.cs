dfbc912b40abb663fa8466d7653e026e87553450
packo97 <pasqualegatto97@gmail.com>
2021-04-11 22:20:53 +0200

    Fixed bug in InteractionWithObject.cs

--- Progetto_VR/Assets/Scripts/InteractionWithObject.cs
+++ Progetto_VR/Assets/Scripts/InteractionWithObject.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public class InteractionWithObject : MonoBehaviour
 {
     private GameObject objectInRange;
-
+    private bool isTaken = false;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -16,36 +17,53 @@ public class InteractionWithObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.E))
+        /*
+         * Con la pressione del tasto "E" è possibile prendere gli oggetti che possiedono lo script "Interactable" e sono nel range del collider
+         * Se si possiede un oggetto, premendo il tasto "E" si rilascia l'oggetto nella posizione in cui si trova il player
+         * 
+         */
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            if (objectInRange != null)
+            if (objectInRange != null && !isTaken)
             {
-                Debug.Log("Preso");
                 objectInRange.SetActive(false);
+                isTaken = true;
             }
-            else
+            else if (isTaken)
             {
-                Debug.Log("Droppa");
                 objectInRange.transform.position = gameObject.transform.position;
                 objectInRange.SetActive(true);
+                isTaken = false;
             }
         }
     }
     
     private void OnTriggerEnter(Collider other)
     {
+        /*
+         * Se sei nel range di un oggetto che possiede lo script "Interactable", tienilo in memoria
+         * 
+         */
+        
         GameObject obj = other.gameObject;
 
         if (obj.GetComponent<Interactable>())
         {
-            Debug.Log("L'oggetto " + other.gameObject.name + "è interagibile");
             objectInRange = obj;
         }
     }
-    /*
+    
     private void OnTriggerExit(Collider other)
     {
-        objectInRange = null;
+        /*
+         * Se esci dal range di un oggetto che possiede lo script "Interactable", non tenerlo più in memoria
+         */
+        GameObject obj = other.gameObject;
+
+        if (obj.GetComponent<Interactable>())
+        {
+            objectInRange = null;
+        }
     }
-    */
+    
 }
