a5dadaa963fc68b8a900522233621c2cfa3effc1
Tiziana Oliviero <42492868+TizianaOliviero@users.noreply.github.com>
2021-04-20 17:51:37 +0200

    Bozza generatore
    Abbozzata interazione con il generatore

--- Progetto_VR/Assets/Scripts/InteractionWithObject.cs
+++ Progetto_VR/Assets/Scripts/InteractionWithObject.cs
@@ -7,11 +7,12 @@ public class InteractionWithObject : MonoBehaviour
 {
     private GameObject interactable_object;
     private bool isTaken = false;
+    private Transfomation transformation;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        transformation = GetComponent<Transfomation>();
     }
 
     // Update is called once per frame
@@ -29,6 +30,8 @@ public class InteractionWithObject : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.E))
         {
+
+
             if (isTaken)
             {
                 interactable_object.transform.position = transform.TransformPoint(0, 1, 1);
@@ -51,18 +54,63 @@ public class InteractionWithObject : MonoBehaviour
                         {
                             isInFrontOfMe = true;
                         }
-                        
+
                         if (!isTaken && isInFrontOfMe)
                         {
                             interactable_object.SetActive(false);
                             isTaken = true;
                         }
-                    
+
                     }
                 }
             }
-            
-            
+
+
+            if (transformation.transf == Transfomation.Tranformation.Rame) { 
+                
+                Collider[] hitColliders = Physics.OverlapSphere(this.transform.position, 2);
+                foreach (Collider hitCollider in hitColliders)
+                {
+                    // se interagiamo con un electrical
+                    if (hitCollider.gameObject.GetComponent<Electrical>() != null)
+                    {
+                        Transform[] childrenOfElectrical = hitCollider.gameObject.GetComponentsInChildren<Transform>(true);
+                        bool isElectricalActive=false;
+                        //vediamo se l'electrical è attivo
+                        for (int i = 0; i < childrenOfElectrical.Length; i++)
+                            if (childrenOfElectrical[i].GetComponent<ParticleSystem>() != null)
+                                isElectricalActive = childrenOfElectrical[i].gameObject.activeSelf;
+
+                        Transform[] children = GetComponentsInChildren<Transform>(true);
+                        if (isElectricalActive)
+                        {
+                            Debug.Log("electrical acceso");
+                            
+                            //se interagiamo con l'electrical active ci attiviamo le nostre particelle
+                            for (int i = 0; i < children.Length; i++)
+                                if (children[i].GetComponent<ParticleSystem>())
+                                    children[i].gameObject.SetActive(true);
+                        }
+                        else //se interagiamo con l'electrical spento ma noi siamo carichi lo attiviamo
+                        {
+                            Debug.Log("electrical spento");
+                            bool value = false;
+                            //vediamo se noi siamo carichi
+                            for (int i = 0; i < children.Length; i++)
+                                if (children[i].GetComponent<ParticleSystem>())
+                                    value = children[i].gameObject.activeSelf;
+
+                            if (value)
+                            {
+                                for (int i = 0; i < childrenOfElectrical.Length; i++)
+                                    if (childrenOfElectrical[i].GetComponent<ParticleSystem>())
+                                        childrenOfElectrical[i].gameObject.SetActive(true);
+                            }
+                        }
+                    } 
+
+                }
+            }
         }
     }
 }
